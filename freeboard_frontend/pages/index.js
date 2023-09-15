761682d70edd2bba3c6736cee818db7e4a3b368f
deenuit113 <leejh5314@gmail.com>
2023-09-15 22:54:19 +0900

    React-portfolio-03 (FreeBoard)

--- freeboard_frontend/pages/index.js
+++ freeboard_frontend/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>fuck2</div>
+      <div>2</div>
     </>
   )
 }
