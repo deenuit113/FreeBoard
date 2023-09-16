20fd9da75f01c3dddf5d4c10b1b540839d33ae65
deenuit113 <leejh5314@gmail.com>
2023-09-16 23:27:01 +0900

    Added React class 05-07 to 08

--- class/pages/05-08-dynamic-routed-board-query/qqq/index.js
+++ class/pages/05-08-dynamic-routed-board-query/qqq/index.js
@@ -1,4 +1,5 @@
 import { useQuery, gql } from "@apollo/client"
+import { useRouter } from "next/router"
 
 const FETCH_BOARD = gql`
     query fetchBoard($number: Int){
@@ -10,10 +11,13 @@ const FETCH_BOARD = gql`
     }
 `
 
-export default function StaticRoutedPage() {
+export default function DynamicRoutedPage() {
+
+    const router = useRouter()
+
     const {data} = useQuery(FETCH_BOARD, {
         variables: {
-            number: 1
+            number: Number(router.query.qqq)
         }
     })
 
@@ -21,7 +25,7 @@ export default function StaticRoutedPage() {
 
     return (
         <>
-            <div>1번 페이지 이동 완료</div>
+            <div>{router.query.qqq}번 페이지 이동 완료</div>
             <div>작성자: {data ? data.fetchBoard.writer : "로딩중"} </div>
             <div>제목: {data && data.fetchBoard.title} </div>
             <div>내용: {data?.fetchBoard.contents} </div>
