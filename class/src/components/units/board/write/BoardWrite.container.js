93e10e505bd7026264463d36ea2e65e928f2d6a0
deenuit113 <leejh5314@gmail.com>
2023-09-17 13:37:56 +0900

    Refactored React class 06-01

--- class/src/components/units/board/write/BoardWrite.container.js
+++ class/src/components/units/board/write/BoardWrite.container.js
@@ -1,23 +1,15 @@
 import BoardWriteUI from "./BoardWrite.presenter"
-import { gql, useMutation } from "@apollo/client"
+import { useMutation } from "@apollo/client"
 import { useState } from "react"
+import { CREATE_BOARD } from "./BoardWrite.queries"
 
-const CREATE_BOARD = gql`
-    mutation createBoard($writer: String, $title: String, $contents: String){
-        createBoard(writer: $writer, title: $title, contents:$contents){
-            _id
-            number
-            message
-        }
-    }
-`
+
 
 export default function BoardWrite() {
 
     const [writer, setWriter] = useState("")
     const [title, setTitle] = useState("")
     const [contents, setContents] = useState("")
-
     const [내함수] = useMutation(CREATE_BOARD)
 
     const onClickSubmit = async () => {
